723f0c26ab013eb8e671e0bcb283bbc05f8c2bce
MaxPalamarchuk23 <129264764+MaxPalamarchuk23@users.noreply.github.com>
2023-06-02 13:08:38 +0300

    Тести

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Recipe
+from .views import RecipeListView, RecipeDescriptionView
 
-# Create your tests here.
+
+class RecipeListViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+       
+        self.recipe1 = Recipe.objects.create(
+            recipe_name='Recipe 1',
+            recipe_description='Recipe 1 description'
+        )
+        self.recipe2 = Recipe.objects.create(
+            recipe_name='Recipe 2',
+            recipe_description='Recipe 2 description'
+        )
+
+    def test_recipe_list_view(self):
+        url = reverse('recipe-list')
+        request = self.factory.get(url)
+        request.user = self.user
+
+        response = RecipeListView.as_view()(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'admin_templates/product_list.html')
+        self.assertContains(response, self.recipe1.recipe_name)
+        self.assertContains(response, self.recipe2.recipe_name)
+
+
